755ec865cf4be2b3f650f63b440eb6cbfd733385
Necati Meral <necati_meral@yahoo.de>
2021-06-28 19:24:50 +0200

    Enabled ninox update on user update

--- src/packages/ninox_user/ninox_user.php
+++ src/packages/ninox_user/ninox_user.php
@@ -11,16 +11,16 @@ defined('_JEXEC') or die('Restricted access');
 jimport('joomla.plugin.plugin');
 
 class plgUserNinox_user extends JPlugin {
-	protected $article_id;
 	
 	public function __construct(& $subject, $config) {
 		parent::__construct($subject, $config);
 	}
 		
 	public function onUserAfterSave($user, $isnew, $success, $msg) {
-		if($isnew && $success)
+		if($success)
 		{
 			require_once(JPATH_SITE.'/administrator/components/com_ninox/helpers/ninox.php');
+			
 			$config = NinoxHelper::getParams();
 			if ($config->get('auto_sync') && $config->get('apikey')) 
 			{
@@ -30,9 +30,11 @@ class plgUserNinox_user extends JPlugin {
 				$first_name = implode(" ", $parts);
 				$contacts[] = array(
 					'id' => $user['id'], 
-					'first_name' => $first_name, 
-					'last_name' => $last_name, 
-					'email' => $user['email']
+					'first_name' => $first_name,
+					'last_name' => $last_name,
+					'email' => $user['email'],
+					'registerDate' => $user['registerDate'],
+					'lastVisitDate' => $user['lastVisitDate']
 				);
 
 				NinoxHelper::addContacts($contacts);
